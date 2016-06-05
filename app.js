ec5ff1139c60b537036dd83571e4be633d0e0f66
Philip <molo4nik11@gmail.com>
2016-06-06 01:42:38 +0300

    fix

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ app.get('/stop', function (req, res, next) {
     clearInterval(timeInter);
     actualTime = time;
     botStatus = false;
+    res.send('ok');
 });
 app.get('/log', function (req, res) {
     res.sendfile('debug.log');
