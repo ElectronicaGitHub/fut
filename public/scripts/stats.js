73995d32b4032ac672b7b1de3b288c1a5d67104d
Philip <molo4nik11@gmail.com>
2016-06-09 00:54:58 +0300

    stats

--- public/scripts/stats.js
+++ public/scripts/stats.js
@@ -40,8 +40,8 @@ var svg = d3.select("#graph").append("svg")
     });
     d = {};
     for (var i in data) {
-    	d[moment(data[0].date).startOf('hours').format()] = d[moment(data[0].date).startOf('hours').format()] || 0;
-    	d[moment(data[0].date).startOf('hours').format()]++;
+    	d[moment(data[i].date).startOf('hours').format()] = d[moment(data[i].date).startOf('hours').format()] || 0;
+    	d[moment(data[i].date).startOf('hours').format()]++;
     }
     console.log(d);
 	data = [];
