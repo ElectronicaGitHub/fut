f823be94909729aa1cc3aaeb402565def54f994c
Philip <molo4nik11@gmail.com>
2016-06-18 03:04:27 +0300

    fix

--- trader.js
+++ trader.js
@@ -666,8 +666,8 @@ Trader.prototype.buyMin = function (player, BUYMINCALLBACK) {
 	// ПРОВЕРКА ДЕФОЛТНОЙ ПОВЫШАЮЩЕЙ СТРАТЕГИИ
 	if (self.currentStrategyData.type == 'default') {
 		// проверка на пиздатую скорость и дриблинг
-		if (player.itemData.attributeList[0].value <= self.options.minPlayerSpeed && 
-			player.itemData.attributeList[4].value <= self.options.minPlayerDribling) {
+		if (player.itemData.attributeList[0].value <= self.options.minPlayerSpeed || 
+			player.itemData.attributeList[3].value <= self.options.minPlayerDribling) {
 			console.log('buyMin::TOO LOW SPEED, SKIP THIS PLAYER');
 			self.iterateParams.costs[player.tradeId] = self.iterateParams.costs[player.tradeId] || {};
 			self.iterateParams.costs[player.tradeId].was = true;
