15da8b4e9884a3105abe061bcaa10ff533c55f1d
Philip <molo4nik11@gmail.com>
2016-06-21 02:04:52 +0300

    fix

--- models/player.js
+++ models/player.js
@@ -40,7 +40,7 @@ var Player = new Schema({
 	}
 });
 
-Player.virtual('revenue').get(function() { return this.sellPrice - this.buyPrice * 1.0555; });
+Player.virtual('revenue').get(function() { return this.sellPrice - this.buyPrice * 1.05555; });
 Player.virtual('timeDiff').get(function() {
 	if (this.soldTime && this.created) {
 		var a = moment(this.soldTime);
