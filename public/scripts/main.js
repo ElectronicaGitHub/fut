3426370b219c906c861a3f03bbc991ec72454d23
Philip <molo4nik11@gmail.com>
2016-06-26 18:05:33 +0300

    fix

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -37,7 +37,9 @@ angular.module('fifatrader', []).controller('fifatrader', ['$scope', '$http', fu
 	$scope.todayPlayers = 0;
 	$scope.players = $scope.players.map(function (el) {
 		el.today = moment(el.soldTime).format('DDMM') == moment().format('DDMM');
-		el.today && $scope.todayPlayers++;
+		if (e.todayPlayers) {
+			$scope.todayPlayers++;
+		}
 		return el;
 	});
 	$scope.app = {
